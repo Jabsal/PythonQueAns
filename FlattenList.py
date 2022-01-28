8c25ff8a7ced735697024df18b16ce8c4f65d950
Joseph Aduratola Salu <UI750530@rwe.com>
2022-01-28 05:03:47 +0100

    flatten list

--- FlattenList.py
+++ FlattenList.py
@@ -1,4 +1,17 @@
+# traverse list and store previous val that is not null
+# append val to list
 class Solution:
 
-    def replace_none():
-        pass
+    def replace_none(lst):
+        
+        result, prev = [], None
+
+        for val in lst:
+            if val != None:
+                prev = val
+
+            result.append(prev)
+
+        return result
+
+print('replace_none is', Solution.replace_none([None,1,2,None,None,5,7]))
